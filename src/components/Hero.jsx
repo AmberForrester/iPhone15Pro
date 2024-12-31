b1d5e95c5e6b3bf8ce969304f3058fb69c1e288e
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-31 18:04:05 -0500

    Highlights section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
   }, [])
 
   return (
-    <section className="w-full nav-height bg-black relati">
+    <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">iPhone 15 Pro</p>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,9 @@ export default {
       colors: {
         blue: "#2997FF",
         gray: {
-          DEFAULT: "#86868B",
-          100: "#94928D",
-          200: "#AFAFAF",
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
           300: "#42424570",
         },
         zinc: "#101010",
